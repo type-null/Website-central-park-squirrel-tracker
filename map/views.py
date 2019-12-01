7e84e7ef7e1956e6c6fecbec79fb2d90ec663a17
Zhongyu Zhang <zz2690@columbia.edu>
2019-12-01 21:53:05 +0000

    Modify map.html

--- map/views.py
+++ map/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 from sightings.models import Squirrel
 
 def default_map(request):
-    longitude = [longitude for longitude in Squirrel.objects.all()]
-    latitude = [latitude for latitude in Squirrel.objects.all()]
-    context = {'longitude': longitude, 'latitude': latitude,}
-    return render(request, 'map/map.html', context)
+    return render(request, 'map/map.html', {})
