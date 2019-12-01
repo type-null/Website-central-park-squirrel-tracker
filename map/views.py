1b3a67a58af04c0240cea66a8c4fd94f1d9eac22
Zhongyu Zhang <zz2690@columbia.edu>
2019-12-01 03:26:30 +0000

    Update map.html

--- map/views.py
+++ map/views.py
@@ -4,5 +4,5 @@ from sightings.models import Squirrel
 def default_map(request):
     longitude = [longitude for longitude in Squirrel.objects.all()]
     latitude = [latitude for latitude in Squirrel.objects.all()]
-    context = {longitude, latitude,}
+    context = {'longitude': longitude, 'latitude': latitude,}
     return render(request, 'map/map.html', context)
