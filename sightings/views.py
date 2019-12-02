4e1347449854ef7ee023b6b428638732724bc5c8
Weihang Ren <rwh18@outlook.com>
2019-12-02 19:04:47 +0000

    Display choices in update

--- sightings/views.py
+++ sightings/views.py
@@ -5,10 +5,14 @@ from .models import Squirrel
 def update(request, squirrel_id):
     sighting = Squirrel.objects.get(squirrel_id=squirrel_id)
     if request.method == 'POST':
-        sighting.save()
-    else:
-        return render(request, 'update.html', {'sighting': sighting})
-    return render_to_response('edit.html', {'sighting': sighting})
+        sighitng.longitude.get(request.POST['longitude'])
+        sighitng.latitude.get(request.POST['latitude'])
+        sighitng.shift.get(request.POST['shift'])
+        sighitng.date.get(request.POST['date'])
+        sighitng.age.get(request.POST['age'])
+        sighting.color=request.POST['color']
+        return render(request, 'sightings.html', {'sighting': sighting})
+    return render(request, 'update.html', {'sighting': sighting})
 
 def list(request):
     sightings = Squirrel.objects.order_by('-squirrel_id')
@@ -16,9 +20,11 @@ def list(request):
     return render(request, 'sightings.html', context)
 
 def add(request):
+    sightings = Squirrel.objects.order_by('-squirrel_id')
     return render(request, "Add", {'sightings': sightings}) 
 
 def stats(request):
+    sightings = Squirrel.objects.order_by('-squirrel_id')
     return render(request, "Stats", {'sightings': sightings}) 
 
 def delete(request, squirrel_id):
